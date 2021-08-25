df3ef5dea7d5ba118e23c94fc4e1e6a120496ad9
Alexandre Morais <alexandre_ferreira_gyn@hotmail.com>
2021-08-24 21:14:21 -0300

    Finalizado desafio e teste passado em 100%

--- src/index.js
+++ src/index.js
@@ -20,6 +20,17 @@ function checksExistsUserAccount(request, response, next) {
   return next();
 }
 
+function checksExistsTodo(request, response, next) {
+  const { id } = request.params;
+  const { user } = request;
+  const todo = user.todos.find(todo => todo.id === id);
+  if (!todo) {
+    return response.status(404).json({ error: 'Todo not exists!' });
+  }
+  request.todo = todo;
+  return next();
+}
+
 app.post('/users', (request, response) => {
   const {name, username} = request.body;
   const userAlreadyExists = users.find(user => user.username === username);
@@ -44,19 +55,40 @@ app.get('/todos', checksExistsUserAccount, (request, response) => {
 });
 
 app.post('/todos', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { title, deadline } = request.body;
+  const { user } = request;
+
+  const todo = { 
+    id: uuidv4(),
+    title,
+    done: false, 
+    deadline: new Date(deadline), 
+    created_at: new Date()
+  }
+
+  user.todos.push(todo);
+
+  return response.status(201).json(todo);
 });
 
-app.put('/todos/:id', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+app.put('/todos/:id', checksExistsUserAccount, checksExistsTodo, (request, response) => {
+  const { title, deadline } = request.body;
+  const { todo } = request;
+  todo.title = title;
+  todo.deadline = new Date(deadline);
+  return response.status(200).json(todo);
 });
 
-app.patch('/todos/:id/done', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+app.patch('/todos/:id/done', checksExistsUserAccount, checksExistsTodo, (request, response) => {
+  const { todo } = request;
+  todo.done = true;
+  return response.status(200).json(todo);
 });
 
-app.delete('/todos/:id', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+app.delete('/todos/:id', checksExistsUserAccount, checksExistsTodo, (request, response) => {
+  const { user, todo } = request;
+  user.todos.splice(todo ,1);
+  return response.status(204).send();  
 });
 
 module.exports = app;
